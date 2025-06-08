98e0917e640fa9aa6a556e8218d6502f55106fb5
shibishwar <sukumar.shibi@gmail.com>
2025-06-08 15:17:56 +0530

    Add Comments

--- src/lib/features/searchSlice.js
+++ src/lib/features/searchSlice.js
@@ -1,19 +1,26 @@
+// Import the createSlice function from Redux Toolkit
 import { createSlice } from "@reduxjs/toolkit";
 
+// Define the initial state for the slice
 const initialState = {
-    value: "",
+    value: "", // This will hold the current search input or value
 };
 
+// Create the slice using createSlice
 export const searchSlice = createSlice({
-    name: "search",
-    initialState,
+    name: "search", // Name of the slice, used as a key in the Redux store
+    initialState, // Initial state defined above
     reducers: {
+        // Define a reducer named "submit"
+        // It updates the state's "value" with the payload from the dispatched action
         submit: (state, action) => {
             state.value = action.payload;
         },
     },
 });
 
+// Export the "submit" action so it can be dispatched from components
 export const { submit } = searchSlice.actions;
 
+// Export the reducer to be added to the Redux store
 export default searchSlice.reducer;
