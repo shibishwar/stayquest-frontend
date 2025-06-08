925d81cb7799311c63d26fe5221caf426711cc14
shibishwar <sukumar.shibi@gmail.com>
2025-06-08 15:17:36 +0530

    Code clean

--- src/lib/features/searchSlice.js
+++ src/lib/features/searchSlice.js
@@ -14,18 +14,6 @@ export const searchSlice = createSlice({
     },
 });
 
-// export const searchSlice = createSlice({
-//     name: "search",
-//     initialState,
-//     reducers: {
-//         submit: (state, action) => {
-//             state.destination = action.payload.destination;
-//             state.experience = action.payload.experience;
-//             state.hotelType = action.payload.hotelType;
-//         },
-//     },
-// });
-
 export const { submit } = searchSlice.actions;
 
 export default searchSlice.reducer;
