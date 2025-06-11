279d1fc672dcbdc2a683b3bf0e79335ff7e5073e
shibishwar <sukumar.shibi@gmail.com>
2025-06-11 22:05:30 +0530

    Add comments

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,10 @@
+// Import clsx: a utility for conditionally joining classNames together
 import { clsx } from "clsx";
+
+// Import twMerge: a utility to intelligently merge Tailwind CSS class names
 import { twMerge } from "tailwind-merge";
 
+// Combines class names using `clsx`, then resolves Tailwind CSS conflicts using `twMerge`
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
 }
